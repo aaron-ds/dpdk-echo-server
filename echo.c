38d64a11d134c35c7ed8004a58ededd0175ae0c2
Aaron <2606144+aaron-ds@users.noreply.github.com>
2024-05-29 14:05:43 +0000

    Initialise eal

--- echo.c
+++ echo.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <rte_eal.h>
+#include <rte_common.h>
 
-int main(int argc, char *argvi[]) {
-    return 1;
+int main(int argc, char *argv[]) {
+	int ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+	return 1;
 }
